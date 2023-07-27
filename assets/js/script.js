aec235a57acaf6cf6736242e0ba78d0c049c2312
JR728 <135184435+JR728@users.noreply.github.com>
2023-07-26 21:20:46 -0500

    added articles imgs and links in articles section

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+async function fetchAndDisplayArticles() {
+    const url = 'https://flixster.p.rapidapi.com/news/list';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c48d143b66msh6a6e77b93d1ddd0p132e84jsn2c5243fa1c06',
+            'X-RapidAPI-Host': 'flixster.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        const articlesContainer = document.getElementById('articles-container');
+
+        let count = 0;
+        // Loop through the articles and create the article elements to append to the container
+        data.data.newsStories.forEach(article => {
+                if (count < 6) {
+                    const articleElement = createArticleElement(article);
+                    articlesContainer.appendChild(articleElement);
+                    count++;
+                } else {
+                    return;
+                }
+            
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+// Helper function to create an article element
+function createArticleElement(article) {
+    const articleElement = document.createElement('article');
+
+    const titleElement = document.createElement('h2');
+    titleElement.innerHTML = article.title;
+    articleElement.appendChild(titleElement);
+
+    const imageElement = document.createElement('img');
+    imageElement.src = article.mainImage.url;
+    articleElement.appendChild(imageElement);
+    imageElement.height=100;
+    imageElement.width=150;
+
+    const linkElement = document.createElement('a');
+    linkElement.href = article.link;
+    linkElement.textContent = 'Read More';
+    articleElement.appendChild(linkElement);
+
+    return articleElement;
+}
+document.addEventListener('DOMContentLoaded', fetchAndDisplayArticles);
